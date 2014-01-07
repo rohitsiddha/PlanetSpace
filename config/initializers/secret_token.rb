b28b74519f38a1ca71e95c50da8d6c0cc731b785
rohit <rohit@ubuntu.ubuntu-domain>
2014-01-08 01:15:12 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanetSpace::Application.config.secret_token = 'a03c35c5ea1d11faf2609c04d4cab299b287892c3453f26e8bdc049b2cea74bbea9755583a365e40b3128b38c5b5387cd1ed766c851af5ee57713ed4a6b2be65'
