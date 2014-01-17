a19e9d3f1ea0d250855310e1dccf277fabe84765
rohitsiddha <rohit.siddha@gmail.com>
2014-01-17 15:24:31 +0530

    removed default test

--- test/unit/helpers/space_vehicles_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SpaceVehiclesHelperTest < ActionView::TestCase
-end
--- test/unit/shared_vehicle_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SharedVehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
